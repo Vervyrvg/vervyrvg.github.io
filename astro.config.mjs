b69b2d6c31e59cfe6ae0cfd470e01c6131ef3338
Vervyrvg <emprendimientorosayvictor@gmail.com>
2025-03-25 23:24:35 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vervyrvg.github.io', // Agrega la URL de tu dominio aquí
   integrations: [tailwind(), react()],
 });
