0e17ec5224d58778ae4ebba4f3cb8b4fbd28d050
Vervyrvg <emprendimientorosayvictor@gmail.com>
2025-04-06 06:24:13 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
@@ -8,24 +8,18 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Vervyrvg.github.io/',  // URL de tu sitio
-  base: '/Vervyrvg.github.io/',         // Base URL para GitHub Pages
-  integrations: [
-    mdx(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon(),
-    sitemap()
-  ],
-  markdown: {
-    shikiConfig: {
-      theme: 'plastic',
-      wrap: true,
+    site: 'https://vervyrvg.github.io',
+    base: '/',
+    integrations: [mdx(), react(), tailwind({
+        applyBaseStyles: false,
+    }), icon(), sitemap()],
+    markdown: {
+        shikiConfig: {
+            theme: 'plastic',
+            wrap: true,
+        },
     },
-  },
-  experimental: {
-    svg: true,
-  },
+    experimental: {
+        svg: true,
+    }
 });
