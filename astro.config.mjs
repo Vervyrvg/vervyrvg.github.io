7833f744936d2867f6a05bde6d69b3ff1717e149
Vervyrvg <emprendimientorosayvictor@gmail.com>
2025-04-06 05:44:35 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,27 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react'; // Asegúrate de que esta línea esté presente
+// @ts-check
+import {defineConfig} from 'astro/config';
+import mdx from "@astrojs/mdx";
 
-const siteUrl = 'https://Vervyrvg.github.io/';
-const baseUrl = '/Vervyrvg.github.io';
+import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  site: siteUrl,
-  base: baseUrl,
-  integrations: [
-    tailwind(),
-    react(), // React integrado
-  ],
-  build: {
-    rollupOptions: {
-      // Configuración adicional si es necesario
-      external: ['virtual:astro-icon'], // Si tienes módulos virtuales, asegúrate de gestionarlos adecuadamente
+    site: 'https://Vervyrvg.github.io/',
+    integrations: [mdx(), react(), tailwind({
+        applyBaseStyles: false,
+    }), icon(), sitemap()],
+    markdown: {
+        shikiConfig: {
+            theme: 'plastic',
+            wrap: true,
+        },
     },
-  },
-  vite: {
-    // Puedes agregar configuraciones específicas de Vite si es necesario
-    server: {
-      hmr: true, // Habilitar HMR si estás desarrollando en local
-    },
-  },
-  // Puedes añadir configuraciones adicionales de Browserslist aquí si es necesario
-  // browserslist: ">0.2%, last 2 versions, Firefox ESR, not dead"
-});
+    experimental: {
+        svg: true,
+    }});
