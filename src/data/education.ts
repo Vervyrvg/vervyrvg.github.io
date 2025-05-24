04f2bca5619357fe58d0779f4916e5a050132a3a
Vervyrvg <emprendimientorosayvictor@gmail.com>
2025-05-23 20:31:08 -0600

    Update education.ts

--- src/data/education.ts
+++ src/data/education.ts
@@ -10,15 +10,15 @@ interface Education {
 
 const  education: Education[] = [
     {
-        title: "Universidad Tecnológica de Nezahualcóyotl",
-        startDate: "2013-02-05",
-        endDate: "2017-06-14",
-        school: "Ing. Tecnologías de la información y comunicación",
-        location: "Cto. Rey Nezahualcóyotl Mz 010, Benito Juarez, Nezahualcóyotl Estado de México,",
-        description: "Developed and maintained web applications using React, NodeJs, and MongoDB.",
-        currentUni: true,
+        
+    title: "Universidad Tecnológica de Nezahualcóyotl",
+    startDate: "2013-02-05",
+    endDate: "2017-06-14",
+    school: "Ing. Tecnologías de la información y comunicación",
+    location: "Cto. Rey Nezahualcóyotl Mz 010, Benito Juarez, Nezahualcóyotl, Estado de México",
+    description: "Adquirí conocimientos en ciberseguridad, especializándome en seguridad perimetral y protección de redes, fortaleciendo habilidades en análisis de riesgos, prevención de vulnerabilidades y configuración de sistemas seguros.",
+    currentUni: false,
     },
     
 ];
 
-export default education;
