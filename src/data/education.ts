aad3b37e171a842660eeeba53166985be77c02e2
Vervyrvg <emprendimientorosayvictor@gmail.com>
2025-05-23 20:56:19 -0600

    Update education.ts

--- src/data/education.ts
+++ src/data/education.ts
@@ -15,7 +15,7 @@ const education: Education[] = [
         endDate: "2017-06-14",
         school: "Ing. Tecnologías de la información y comunicación",
         location: "Cto. Rey Nezahualcóyotl Mz 010, Benito Juarez, Nezahualcóyotl, Estado de México",
-        description: "Adquirí conocimientos en ciberseguridad, especializándome en seguridad perimetral y protección de redes, fortaleciendo habilidades en análisis de riesgos, prevención de vulnerabilidades y configuración de sistemas seguros.",
+        description: "Adquirí conocimientos de Ciberseguridad.",
         currentUni: false,
     },
 ];
