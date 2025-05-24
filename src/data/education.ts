d6c4919ea0795a2848c69a4244bda18368e9e10c
Vervyrvg <emprendimientorosayvictor@gmail.com>
2025-05-23 20:47:53 -0600

    Update education.ts

--- src/data/education.ts
+++ src/data/education.ts
@@ -10,14 +10,25 @@ interface Education {
 
 const  education: Education[] = [
     {
+        interface Education {
+    title: string;
+    startDate: string;
+    endDate: string | null; // Puede ser null si aún estás estudiando
+    school: string;
+    location: string;
+    description: string;
+    currentUni: boolean;
+}
+
+const education: Education[] = [
+    {
         title: "Universidad Tecnológica de Nezahualcóyotl",
-        startDate: "2013-02-09",
-        endDate: "2017-10-18",
-        school: "University Name",
-        location: "Cto. Rey Nezahualcóyotl Mz 010, Benito Juarez, Nezahualcóyotl Estado de México,",
+        startDate: "2013-02-05",
+        endDate: "2017-06-14",
+        school: "Ing. Tecnologías de la información y comunicación",
+        location: "Cto. Rey Nezahualcóyotl Mz 010, Benito Juarez, Nezahualcóyotl, Estado de México",
         description: "Adquirí conocimientos en ciberseguridad, especializándome en seguridad perimetral y protección de redes, fortaleciendo habilidades en análisis de riesgos, prevención de vulnerabilidades y configuración de sistemas seguros.",
         currentUni: false,
     },
-    
 ];
 
