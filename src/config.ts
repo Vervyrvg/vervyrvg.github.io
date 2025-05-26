64b5a35212f33f7e6a7929ceeaa8af966ffab32e
Vervyrvg <emprendimientorosayvictor@gmail.com>
2025-05-26 10:36:45 -0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // config.ts - Archivo de configuración corregido
-export const SITE = {
+export const SITE = 
+{
     website: "https://vervyrvg.github.io/", // replace this with your deployed domain
     title: "Portafolio",
     description: "Especialista, Gestión Administración e Implementación de Soluciones en Ciberseguridad - SOC Senior",
@@ -7,14 +8,14 @@ export const SITE = {
     ogImage: "/og-image.webp",
     logo: "logo",
     logoText: "Portafolio",
-    lang: "es", // Cambiado a español
+    lang: "es",
     favicon: "/favicon.png",
     repository: "https://github.com/Vervyrvg/vervyrvg.github.io",
     author: "Ing. Victor Eduardo Resendiz Villegas",
     profile: "https://vervyrvg.github.io/",
 }
-
-export const ME = {
+export const ME = 
+{
     name: "Victor Eduardo Resendiz Villegas",
     profession: "Especialista, Gestión Administración e Implementación de Soluciones en Ciberseguridad - SOC Senior",
     profileImage: "pp2.png",
@@ -38,9 +39,10 @@ export const ME = {
         {
             value: 4,
             description: "Herramientas o Scripts Desarrollados" // Corregido: "Script's" -> "Scripts"
-        }
+        },
     ],
-    contactInfo: {
+    contactInfo: 
+    {
         email: "victorresendizv@outlook.com",
         linkedin: "https://www.linkedin.com/in/victor-eduardo-resendiz-villegas-910a80290",
         github: "https://github.com/Vervyrvg/vervyrvg.github.io",
@@ -49,8 +51,7 @@ export const ME = {
         cartaDoc: "carta.pdf",
     },
     aboutMe: "Soy un ingeniero titulado en Tecnologías de la Información y Comunicación, con +8 años de experiencia especializada en administración e implementación de soluciones en Ciberseguridad. Mi enfoque profesional se basa en automatizar y optimizar procesos para ofrecer soluciones innovadoras que fortalezcan la protección de sistemas críticos. Durante mi carrera, he liderado proyectos que integran seguridad como un habilitador estratégico de innovación, transformando cada componente de protección en un valor agregado que impulsa el crecimiento sostenible. Con una mentalidad orientada a resultados y una pasión por la tecnología, estoy preparado para enfrentar los desafíos del entorno actual. Mis soluciones combinan análisis predictivo avanzado con marcos de respuesta proactivos, garantizando un nivel de seguridad adaptable y eficiente que potencia la competitividad.",
-}
-
+    }
 export const SOCIALS = [
     {
         name: "WhatsApp",
