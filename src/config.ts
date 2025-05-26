5037cdfa429c09b770beab844d91ce79018c47e7
Vervyrvg <emprendimientorosayvictor@gmail.com>
2025-05-26 10:50:41 -0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
-// config.ts
-export const SITE = {
-    website: "https://vervyrvg.github.io/", // replace this with your deployed domain
+export const SITE = 
+{
+    website: "https://vervyrvg.github.io/",
     title: "Portafolio",
     description: "Especialista, Gestión Administración e Implementación de Soluciones en Ciberseguridad - SOC Senior",
     tags: ["portfolio"],
     ogImage: "/og-image.webp",
     logo: "logo",
     logoText: "Portafolio",
-    lang: "es",
+    lang: "español",
     favicon: "/favicon.png",
     repository: "https://github.com/Vervyrvg/vervyrvg.github.io",
     author: "Ing. Victor Eduardo Resendiz Villegas",
@@ -37,10 +37,11 @@ export const ME = {
         },
         {
             value: 4,
-            description: "Herramientas o Scripts Desarrollados"
-        }
+            description: "Herramientas Desarrolladas"
+        },
     ],
-    contactInfo: {
+    contactInfo: 
+    {
         email: "victorresendizv@outlook.com",
         linkedin: "https://www.linkedin.com/in/victor-eduardo-resendiz-villegas-910a80290",
         github: "https://github.com/Vervyrvg/vervyrvg.github.io",
@@ -50,7 +51,6 @@ export const ME = {
     },
     aboutMe: "Soy un ingeniero titulado en Tecnologías de la Información y Comunicación, con +8 años de experiencia especializada en administración e implementación de soluciones en Ciberseguridad. Mi enfoque profesional se basa en automatizar y optimizar procesos para ofrecer soluciones innovadoras que fortalezcan la protección de sistemas críticos. Durante mi carrera, he liderado proyectos que integran seguridad como un habilitador estratégico de innovación, transformando cada componente de protección en un valor agregado que impulsa el crecimiento sostenible. Con una mentalidad orientada a resultados y una pasión por la tecnología, estoy preparado para enfrentar los desafíos del entorno actual. Mis soluciones combinan análisis predictivo avanzado con marcos de respuesta proactivos, garantizando un nivel de seguridad adaptable y eficiente que potencia la competitividad.",
 }
-
 export const SOCIALS = [
     {
         name: "WhatsApp",
@@ -99,5 +99,4 @@ export const SOCIALS = [
         url: "https://tiktok.com/vicbox.dev",
         icon: "tiktok-fill",
         show: false
-    }
-]
+    },
